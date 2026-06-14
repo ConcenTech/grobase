116419377ae9e6db2844c6eb9e91ba011e7fee75
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2026-06-14 20:36:00 +0100

    chore: app constants

---
+++ app/lib/core/constants.dart
@@ -0,0 +1,9 @@
+const kAppTitle = 'Grobase';
+const kAppScheme = 'grobase';
+const kAppSchemeUrl = '$kAppScheme://';
+
+const kRequired = 'Required';
+
+const secondsInHour = 3600;
+
+const kDefaultSpacing = 8.0;
