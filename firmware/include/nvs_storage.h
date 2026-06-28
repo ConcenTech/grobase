5d2338a6e2b0b3eef5209e3d6e929f5493969bd2
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2026-06-28 21:53:17 +0100

    refactor: remove unused Supabase key functions and enhance NVS storage with debug logging

--- firmware/include/nvs_storage.h
+++ firmware/include/nvs_storage.h
@@ -19,9 +19,6 @@ bool nvsSetGatewayId(const String &id);
 bool nvsGetSupabaseUrl(String &outUrl);
 bool nvsSetSupabaseUrl(const String &url);
 
-bool nvsGetSupabaseAnonKey(String &outKey);
-bool nvsSetSupabaseAnonKey(const String &key);
-
 bool nvsGetInverterId(String &outId);
 bool nvsSetInverterId(const String &id);
 
--- firmware/src/nvs_storage.cpp
+++ firmware/src/nvs_storage.cpp
@@ -2,21 +2,27 @@
 
 #include "nvs_storage.h"
 
+#include "debug_print.h"
 #include <Preferences.h>
 
 static Preferences g_prefs;
+static bool g_nvsOpen = false;
 static const char *NVS_NAMESPACE = "grobase";
 static const char *KEY_WIFI_SSID = "wifi_ssid";
 static const char *KEY_WIFI_PASSWORD = "wifi_pass";
 static const char *KEY_DEVICE_SECRET = "device_secret";
 static const char *KEY_GATEWAY_ID = "gateway_id";
 static const char *KEY_SUPABASE_URL = "supabase_url";
-static const char *KEY_SUPABASE_ANON_KEY = "supabase_anon";
+// static const char *KEY_SUPABASE_ANON_KEY = "supabase_anon";
 static const char *KEY_INVERTER_ID = "inverter_id";
 static const char *KEY_EXPECTED_INVERTER_SN = "expected_sn";
 
 bool nvsBegin() {
-  return g_prefs.begin(NVS_NAMESPACE, false);
+  if (g_nvsOpen) {
+    return true;
+  }
+  g_nvsOpen = g_prefs.begin(NVS_NAMESPACE, false);
+  return g_nvsOpen;
 }
 
 bool nvsGetWifiCredentials(String &outSsid, String &outPassword) {
@@ -34,6 +40,7 @@ bool nvsSetWifiCredentials(const String &ssid, const String &password) {
   }
   g_prefs.putString(KEY_WIFI_SSID, ssid);
   g_prefs.putString(KEY_WIFI_PASSWORD, password);
+  DEBUG_PRINTF("NVS: wifi credentials stored for ssid=%s\n", ssid.c_str());
   return true;
 }
 
@@ -50,6 +57,7 @@ bool nvsSetDeviceSecret(const String &secret) {
     return false;
   }
   g_prefs.putString(KEY_DEVICE_SECRET, secret);
+  DEBUG_PRINTLN("NVS: device secret stored");
   return true;
 }
 
@@ -66,6 +74,7 @@ bool nvsSetGatewayId(const String &id) {
     return false;
   }
   g_prefs.putString(KEY_GATEWAY_ID, id);
+  DEBUG_PRINTF("NVS: gateway_id stored (%s)\n", id.c_str());
   return true;
 }
 
@@ -82,21 +91,7 @@ bool nvsSetSupabaseUrl(const String &url) {
     return false;
   }
   g_prefs.putString(KEY_SUPABASE_URL, url);
-  return true;
-}
-
-bool nvsGetSupabaseAnonKey(String &outKey) {
-  if (!g_prefs.isKey(KEY_SUPABASE_ANON_KEY)) return false;
-  outKey = g_prefs.getString(KEY_SUPABASE_ANON_KEY, "");
-  return outKey.length() > 0;
-}
-
-bool nvsSetSupabaseAnonKey(const String &key) {
-  if (key.length() == 0) {
-    if (g_prefs.isKey(KEY_SUPABASE_ANON_KEY)) g_prefs.remove(KEY_SUPABASE_ANON_KEY);
-    return false;
-  }
-  g_prefs.putString(KEY_SUPABASE_ANON_KEY, key);
+  DEBUG_PRINTF("NVS: supabase_url stored (%s)\n", url.c_str());
   return true;
 }
 
@@ -113,6 +108,7 @@ bool nvsSetInverterId(const String &id) {
     return false;
   }
   g_prefs.putString(KEY_INVERTER_ID, id);
+  DEBUG_PRINTF("NVS: inverter_id stored (%s)\n", id.c_str());
   return true;
 }
 
@@ -129,5 +125,6 @@ bool nvsSetExpectedInverterSn(const String &sn) {
     return false;
   }
   g_prefs.putString(KEY_EXPECTED_INVERTER_SN, sn);
+  DEBUG_PRINTF("NVS: expected_inverter_sn stored (%s)\n", sn.c_str());
   return true;
 }
