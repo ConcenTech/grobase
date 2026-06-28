573d9180b9c17a254c6c18837d2bd692bfc96799
Kevin McDermott-Carpenter <kevin.mcdermott-carpenter@concentechltd.co.uk>
2026-06-28 20:14:41 +0100

    feat: update permissions and dependencies for Bluetooth functionality

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -5,6 +5,19 @@ allprojects {
     }
 }
 
+
+
+subprojects {
+    afterEvaluate {
+        if (hasProperty("android")) {
+            when (val androidExtension = extensions.findByName("android")) {
+                is com.android.build.api.dsl.ApplicationExtension -> androidExtension.compileSdk = 36
+                is com.android.build.api.dsl.LibraryExtension -> androidExtension.compileSdk = 36
+            }
+        }
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -22,3 +35,4 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
